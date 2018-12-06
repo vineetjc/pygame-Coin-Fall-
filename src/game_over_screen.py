7c8990eee1cbbaa743e65314708bb2cacc90a1a7
abhinandan <abhinandankainth1999@gmail.com>
2018-12-06 23:48:24 +0530

    highscore[not encrypted]
    added some backgrounds in images/NewBackgrounds

--- src/game_over_screen.py
+++ src/game_over_screen.py
@@ -14,11 +14,22 @@ class Game_over_screen(Screen):
         self.buttons['Back'] =      Button(pygame, res, surface, [20, 360, 300, 50], "Back")
 
     def update(self, events):
+        hisc=open("highscore.txt","r")
+        highscore=hisc.read()
+        maxscore=max(int(highscore),int(self.game_manager.score))
+        if int(highscore)<int(self.game_manager.score):
+            hisc.close()
+            hisc=open("highscore.txt","w")
+            hisc.write(str(self.game_manager.score))
+        hisc.close()
+
         textsurface = self.font.render('Game Over', True, (0, 0, 0))
         textsurface2 = self.font2.render('Score: ' + str(self.game_manager.score), True, (0, 0, 0))
+        textsurface3 = self.font2.render('HighScore: ' + str(maxscore), True, (0, 0, 0))
         self.surface.fill(self.res.BGCOLOR)
         self.surface.blit(textsurface, (20, 0))
         self.surface.blit(textsurface2, (20, 100))
+        self.surface.blit(textsurface3, (20, 150))
 
         for button in self.buttons:
             self.buttons[button].draw()
