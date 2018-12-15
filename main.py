f7d2a768c6457544aa9c88036f0c83eb3178d02b
amrit-choudhary <albela.amrit@gmail.com>
2018-12-15 19:21:46 +0530

    Cleaned main.py. Moved screens switching logic to screens_manager.py

--- main.py
+++ main.py
@@ -3,70 +3,31 @@ import pygame
 from pygame.locals import QUIT, KEYUP
 from src.misc.game_enums import Game_mode, Entity
 from src.managers import *
-from src.game_screens import *
+from src.game_screens.screens_manager import Screen_Manager
 from src.input_package import input_manager
 
 
 def game_loop():
     pygame.init()
+    res = Resources(pygame)
 
     # setup the window display
     size = (1024, 768)
     windowSurface = pygame.display.set_mode(size, 0, 32)
     pygame.display.set_caption('Coin Fall')
+    pygame.display.set_icon(res.logo)
 
-    # initialize resources and game mode
-    res = Resources(pygame)
+    # initialize game variables
     game_clock = pygame.time.Clock()
     game_mode = Game_mode.MAIN_MENU
     game_manager = Game_manager()
     game_manager.set_input(input_manager.Input_Manager())
+    screen_manager = Screen_Manager(pygame, res, windowSurface, size, game_clock, game_manager)
 
-    # set game logo
-    pygame.display.set_icon(res.logo)
-
-    # initialize screens
-    main_menu_screen = Main_menu_screen(pygame, res, windowSurface)
-    game_screen = Game_screen(
-        pygame, res, windowSurface, size, game_clock, game_manager)
-    settings_screen = Settings_screen(pygame, res, windowSurface)
-    game_over_screen = Game_over_screen(
-        pygame, res, windowSurface, game_manager)
-    tutorial_screen = Tutorial_screen(pygame, res, windowSurface)
-    credits_screen = Credits_screen(pygame, res, windowSurface)
-    game_mode_screen = Game_Mode_Screen(
-        pygame, res, windowSurface, game_manager)
-
-    # game loop starts
     while True:
         events = pygame.event.get()
         game_manager.input.update(events)
-
-        if game_mode == Game_mode.MAIN_MENU:
-            game_mode = main_menu_screen.update(events)
-
-        elif game_mode == Game_mode.GAME:
-            game_mode = game_screen.update(events)
-
-        elif game_mode == Game_mode.SETTINGS:
-            game_mode = settings_screen.update(events)
-
-        elif game_mode == Game_mode.GAME_OVER:
-            game_mode = game_over_screen.update(events)
-
-        elif game_mode == Game_mode.TUTORIAL:
-            game_mode = tutorial_screen.update(events)
-
-        elif game_mode == Game_mode.CREDITS:
-            game_mode = credits_screen.update(events)
-
-        elif game_mode == Game_mode.GAME_MODE:
-            game_mode = game_mode_screen.update(events)
-
-        else:
-            pygame.quit()
-            sys.exit()
-
+        game_mode = screen_manager.show(game_mode, events)
         game_clock.tick(60)
 
 
---
+++ src/game_screens/screens_manager.py
@@ -0,0 +1,58 @@
+#########################################
+# Manages all the screens
+#########################################
+from .main_menu_screen import Main_menu_screen
+from .game_screen import Game_screen
+from .settings_screen import Settings_screen
+from .game_over_screen import Game_over_screen
+from .tutorial_screen import Tutorial_screen
+from .credits_screen import Credits_screen
+from .game_mode_screen import Game_Mode_Screen
+from src.misc.game_enums import Game_mode
+import sys
+
+
+class Screen_Manager:
+    def __init__(self, pygame, res, surface, size, game_clock, game_manager):
+        self.pygame = pygame
+        self.init_screens(pygame, res, surface, size, game_clock, game_manager)
+
+    def init_screens(self, pygame, res, surface, size, game_clock, game_manager):
+        ''' Init all the screen, based on params provided by main.py. '''
+        self.main_menu_screen = Main_menu_screen(pygame, res, surface)
+        self.game_screen = Game_screen(pygame, res, surface, size, game_clock, game_manager)
+        self.settings_screen = Settings_screen(pygame, res, surface)
+        self.game_over_screen = Game_over_screen(pygame, res, surface, game_manager)
+        self.tutorial_screen = Tutorial_screen(pygame, res, surface)
+        self.credits_screen = Credits_screen(pygame, res, surface)
+        self.game_mode_screen = Game_Mode_Screen(pygame, res, surface, game_manager)
+
+    def show(self, game_mode, events):
+        ''' Show a screen based on game mode and will return the next game mode. '''
+        return self.game_mode_to_screen(game_mode).update(events)
+
+    def game_mode_to_screen(self, game_mode):
+        if game_mode == Game_mode.MAIN_MENU:
+            return self.main_menu_screen
+
+        elif game_mode == Game_mode.GAME:
+            return self.game_screen
+
+        elif game_mode == Game_mode.SETTINGS:
+            return self.settings_screen
+
+        elif game_mode == Game_mode.GAME_OVER:
+            return self.game_over_screen
+
+        elif game_mode == Game_mode.TUTORIAL:
+            return self.tutorial_screen
+
+        elif game_mode == Game_mode.CREDITS:
+            return self.credits_screen
+
+        elif game_mode == Game_mode.GAME_MODE:
+            return self.game_mode_screen
+
+        else:
+            self.pygame.quit()
+            sys.exit()
