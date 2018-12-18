227ef5a23047efdfdc67ebbb9156b958db2feba7
vineetjc <vineetjc@yahoo.com>
2018-12-18 23:08:41 +0530

    Minor clean up on #49, folder rename, added comments

--- src/managers/sound_manager.py
+++ src/managers/sound_manager.py
@@ -1,10 +1,16 @@
+##########################################
+# manages sounds
+###########################################
+
 import pygame
 
 
 class Play_sound:
 
+    #set up background music
     def __init__(self,background_music):
         self.background_music = pygame.mixer.music.load("res/Music/bg/background_music.mp3")
 
+    #plays on loop
     def play_background_music(self):
         pygame.mixer.music.play(-1)
