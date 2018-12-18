d7aa4841f46f144e896f39f901c0e16237dc51ee
vineetjc <vineetjc@yahoo.com>
2018-12-18 23:41:10 +0530

    Correct bg music path for loading

--- src/managers/sound_manager.py
+++ src/managers/sound_manager.py
@@ -9,7 +9,7 @@ class Play_sound:
 
     #set up background music
     def __init__(self,background_music):
-        self.background_music = pygame.mixer.music.load("res/Music/bg/background_music.mp3")
+        self.background_music = pygame.mixer.music.load("res/music/bg/background_music.mp3")
 
     #plays on loop
     def play_background_music(self):
