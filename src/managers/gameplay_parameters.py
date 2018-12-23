bce06215ef13b0910ef395139af1d87b461a6aae
amrit-choudhary <albela.amrit@gmail.com>
2018-12-23 15:19:40 +0530

    defined all game mode parameters

--- src/managers/gameplay_parameters.py
+++ src/managers/gameplay_parameters.py
@@ -9,8 +9,13 @@ class Gameplay_Parameters:
     def __init__(self):
         self.params_list = {}
 
+        #
+        #
+        #
         # Classic mode parameters
         #
+        # display_name: display name is game introduction screen
+        # introduction: intoduction text in the game introduction screen
         # score_multiplier: -
         # spawn_chance: chance of spawing anything every frame
         # silver_chance: if we have to spawn, chances of silver coin spawning
@@ -19,6 +24,11 @@ class Gameplay_Parameters:
         # Note: all chances in fraction. 0.10 = 10% chance of spawning
 
         self.params_list['classic_easy'] = {
+            'display_name':     'Classic',
+            'introduction':     """
+                                The classic game mode, avoid bombs and 
+                                collect as many coins as possible in 30 seconds.
+                                """,
             'score_multiplier': 1.0,
             'spawn_chance':     0.030,
             'silver_chance':    0.60,
@@ -27,6 +37,11 @@ class Gameplay_Parameters:
         }
 
         self.params_list['classic_medium'] = {
+            'display_name':     'Classic',
+            'introduction':     """
+                                The classic game mode, avoid bombs and 
+                                collect as many coins as possible in 30 seconds.
+                                """,
             'score_multiplier': 3.0,
             'spawn_chance':     0.075,
             'silver_chance':    0.55,
@@ -35,9 +50,151 @@ class Gameplay_Parameters:
         }
 
         self.params_list['classic_hard'] = {
+            'display_name':     'Classic',
+            'introduction':     """
+                                The classic game mode, avoid bombs and 
+                                collect as many coins as possible in 30 seconds.
+                                """,
             'score_multiplier': 5.0,
             'spawn_chance':     0.120,
             'silver_chance':    0.45,
             'gold_chance':      0.15,
             'bomb_chance':      0.30
+        }
+
+        #
+        #
+        #
+        # Infinite mode parameters
+        #
+        # display_name: display name is game introduction screen
+        # introduction: intoduction text in the game introduction screen
+        # score_multiplier: -
+        # spawn_chance: chance of spawing anything every frame
+        # silver_chance: if we have to spawn, chances of silver coin spawning
+        # gold_chance: see above
+        # bomb_chance: see above
+        # Note: all chances in fraction. 0.10 = 10% chance of spawning
+
+        self.params_list['infinite'] = {
+            'display_name':     'Infinite',
+            'introduction':     """
+                                The infinite game mode, avoid bombs and 
+                                collect as many coins as possible without 
+                                the time limit.
+                                """,
+            'score_multiplier': 3.0,
+            'spawn_chance':     0.075,
+            'silver_chance':    0.55,
+            'gold_chance':      0.25,
+            'bomb_chance':      0.20
+        }
+
+        #
+        #
+        #
+        # 1 v 1 mode parameters
+        #
+        # display_name: display name is game introduction screen
+        # introduction: intoduction text in the game introduction screen
+        # score_multiplier: -
+        # spawn_chance: chance of spawing anything every frame
+        # silver_chance: if we have to spawn, chances of silver coin spawning
+        # gold_chance: see above
+        # bomb_chance: see above
+        # Note: all chances in fraction. 0.10 = 10% chance of spawning
+
+        self.params_list['1v1'] = {
+            'display_name':     '1 v 1',
+            'introduction':     """
+                                The 1 v 1 game mode, compete against your 
+                                friend to score more points in a time limit.
+                                """,
+            'score_multiplier': 3.0,
+            'spawn_chance':     0.075,
+            'silver_chance':    0.55,
+            'gold_chance':      0.25,
+            'bomb_chance':      0.20
+        }
+
+        #
+        #
+        #
+        # AI mode parameters
+        #
+        # display_name: display name is game introduction screen
+        # introduction: intoduction text in the game introduction screen
+        # score_multiplier: -
+        # spawn_chance: chance of spawing anything every frame
+        # silver_chance: if we have to spawn, chances of silver coin spawning
+        # gold_chance: see above
+        # bomb_chance: see above
+        # Note: all chances in fraction. 0.10 = 10% chance of spawning
+
+        self.params_list['ai'] = {
+            'display_name':     'AI',
+            'introduction':     """
+                                The AI game mode, design you own AI to 
+                                compete for AI highscore.
+                                """,
+            'score_multiplier': 3.0,
+            'spawn_chance':     0.075,
+            'silver_chance':    0.55,
+            'gold_chance':      0.25,
+            'bomb_chance':      0.20
+        }
+
+        #
+        #
+        #
+        # Hardcore mode parameters
+        #
+        # display_name: display name is game introduction screen
+        # introduction: intoduction text in the game introduction screen
+        # score_multiplier: -
+        # spawn_chance: chance of spawing anything every frame
+        # silver_chance: if we have to spawn, chances of silver coin spawning
+        # gold_chance: see above
+        # bomb_chance: see above
+        # Note: all chances in fraction. 0.10 = 10% chance of spawning
+
+        self.params_list['hardcore'] = {
+            'display_name':     'Hardcore',
+            'introduction':     """
+                                The hardcore game mode, try to survive 
+                                long as you can in a rain on bombs.
+                                """,
+            'score_multiplier': 3.0,
+            'spawn_chance':     0.075,
+            'silver_chance':    0.55,
+            'gold_chance':      0.25,
+            'bomb_chance':      0.20
+        }
+
+        #
+        #
+        #
+        # Heist mode parameters
+        #
+        # display_name: display name is game introduction screen
+        # introduction: intoduction text in the game introduction screen
+        # score_multiplier: -
+        # spawn_chance: chance of spawing anything every frame
+        # silver_chance: if we have to spawn, chances of silver coin spawning
+        # gold_chance: see above
+        # bomb_chance: see above
+        # Note: all chances in fraction. 0.10 = 10% chance of spawning
+
+        self.params_list['heist'] = {
+            'display_name':     'Heist',
+            'introduction':     """
+                                The Heist game mode, collect as many 
+                                coins as you can in a rain of coins but 
+                                don't miss too many coins in a row.
+                                """,
+            'score_multiplier': 3.0,
+            'spawn_chance':     0.075,
+            'silver_chance':    0.55,
+            'gold_chance':      0.25,
+            'bomb_chance':      0.20
         }
