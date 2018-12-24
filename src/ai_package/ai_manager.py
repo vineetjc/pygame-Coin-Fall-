a0b50ef20b914102f05b75cc9bdf28948471723d
amrit-choudhary <albela.amrit@gmail.com>
2018-12-24 19:06:06 +0530

    Added: 2 example ai

--- src/ai_package/ai_manager.py
+++ src/ai_package/ai_manager.py
@@ -2,13 +2,13 @@
 # AI Manager which acts as interface
 # between the AI and the game
 #########################################
-from .ai_simplex import AI_Simplex
+from .ai_avoid_bomb import AI_Avoid_Bomb
 
 
 class AI_Manager():
 
     def __init__(self):
-        self.ai = AI_Simplex()
+        self.ai = AI_Avoid_Bomb()
 
     def restart(self):
         self.ai.restart()
